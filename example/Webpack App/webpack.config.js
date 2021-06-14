bd1bb383b3b381d8a2b244372026a3b6c0bdff62
Donald <donaldcwl@gmail.com>
2021-06-14 14:06:13 +0800

    feat: add webpack example

---
+++ example/Webpack App/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
