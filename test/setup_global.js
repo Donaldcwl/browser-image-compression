8b9774905125eef108f0ebd1c38e7220fb2ce439
Donald <donaldcwl@gmail.com>
2021-09-26 22:57:03 +0800

    test: setup global variables

--- test/setup_global.js
+++ test/setup_global.js
@@ -1,6 +1,6 @@
 const pkg = require('../package.json');
 
-global.process.env.NODE_ENV = 'production';
-global.process.env.BUILD = 'production';
+global.process.env.NODE_ENV = 'test';
+global.process.env.BUILD = 'test';
 global.__buildDate__ = () => JSON.stringify(new Date());
 global.__buildVersion__ = JSON.stringify(pkg.version);
